0c907e7cb71894d62fce50322043fd3848f5ab4a
root <nathan.pankowsky@gmail.com>
2017-01-12 22:27:04 +0200

    more elegant upload.php params

--- website/upload.php
+++ website/upload.php
@@ -1,21 +1,24 @@
 <?php
-    $Location = 'Location'.':'.$_GET['Location'];
-    $TypeOfSensor = 'TypeOfSensor'.':'.$_GET['TypeOfSensor'];
-    $SensorId = 'SensorId'.':'.$_GET['SensorId'];
-    $TestType = 'TestType'.':'.$_GET['TestType'];
-    $SensorOwner = 'SensorOwner'.':'.$_GET['SensorOwner'];
-    $SensorValue = 'SensorValue'.':'.$_GET['SensorValue'];
-    $ReportedTriggerValue = 'ReportedTriggerValue'.':'.$_GET['ReportedTriggerValue'];
-    $ReportedThresholdValue = 'ReportedThresholdValue'.':'.$_GET['ReportedThresholdValue'];
-    $ReportedNormalValue = 'ReportedNormalValue'.':'.$_GET['ReportedNormalValue'];
-    $SensorUnits = 'SensorUnits'.':'.$_GET['SensorUnits'];
-    $input = $Location." ".$TypeOfSensor." ".$SensorId." ".$SensorOwner." ".$SensorValue." ".$ReportedTriggerValue." ".$ReportedThresholdValue." ".$ReportedNormalValue." ".$SensorUnits;
+    $vars = array(
+        'Location',
+        'TypeOfSensor',
+        'SensorId',
+        'TestType',
+        'SensorOwner',
+        'SensorValue',
+        'ReportedTriggerValue',
+        'ReportedThresholdValue',
+        'ReportedNormalValue',
+        'SensorUnits',
+    );
+    $input = '';
+    foreach($vars as &$value) {
+        $input=$input.$value.':'.$_GET[$value].' ';
+    }
     $command = "python process_input.py $input >> cache";
-    //$input > output.txt";
     echo $input;
     $handle = popen($command, 'r');
     $output = fread($handle, 2096);
     echo $output;
-    //echo $input;
     fclose($handle);
 ?>
