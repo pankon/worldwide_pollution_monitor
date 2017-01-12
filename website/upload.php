06384b29a0da467e1bb592e6bca2c08f9c7a1f50
root <nathan.pankowsky@gmail.com>
2017-01-12 22:13:49 +0200

    updated upload.php

--- website/upload.php
+++ website/upload.php
@@ -1,15 +1,21 @@
 <?php
-    $Location = $_GET['Location']
-    $TypeOfSensor = $_GET['TypeOfSensor']
-    $SensorId = $_GET['SensorId']
-    $TestType = $_GET['TestType']
-    $SensorOwner = $_GET['SensorOwner']
-    $SensorValue = $_GET['SensorValue']
-    $ReportedTriggerValue = $_GET['ReportedTriggerValue']
-    $ReportedThresholdValue = $_GET['ReportedThresholdValue']
-    $ReportedNormalValue = $_GET['ReportedNormalValue']
-    $SensorUnits = $_GET['SensorUnits']
-    $input = '{Location=$Location, TypeOfSensor=$TypeOfSensor, SensorId=$SensorId, SensorOwner=$SensorOwner, SensorValue=$SensorValue, ReportedTriggerValue=$ReportedTriggerValue, ReportedThresholdValue=$ReportedThresholdValue, ReportedNormalValue=$ReportedNormalValue, SensorUnits=$SensorUnits]'
-    $command = "python process_input.py $input > output.txt";
-    popen($command, 'r');
+    $Location = 'Location'.':'.$_GET['Location'];
+    $TypeOfSensor = 'TypeOfSensor'.':'.$_GET['TypeOfSensor'];
+    $SensorId = 'SensorId'.':'.$_GET['SensorId'];
+    $TestType = 'TestType'.':'.$_GET['TestType'];
+    $SensorOwner = 'SensorOwner'.':'.$_GET['SensorOwner'];
+    $SensorValue = 'SensorValue'.':'.$_GET['SensorValue'];
+    $ReportedTriggerValue = 'ReportedTriggerValue'.':'.$_GET['ReportedTriggerValue'];
+    $ReportedThresholdValue = 'ReportedThresholdValue'.':'.$_GET['ReportedThresholdValue'];
+    $ReportedNormalValue = 'ReportedNormalValue'.':'.$_GET['ReportedNormalValue'];
+    $SensorUnits = 'SensorUnits'.':'.$_GET['SensorUnits'];
+    $input = $Location." ".$TypeOfSensor." ".$SensorId." ".$SensorOwner." ".$SensorValue." ".$ReportedTriggerValue." ".$ReportedThresholdValue." ".$ReportedNormalValue." ".$SensorUnits;
+    $command = "python process_input.py $input >> cache";
+    //$input > output.txt";
+    echo $input;
+    $handle = popen($command, 'r');
+    $output = fread($handle, 2096);
+    echo $output;
+    //echo $input;
+    fclose($handle);
 ?>
