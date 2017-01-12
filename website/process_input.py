6f5e02232f91ef863abfba142bfceb5d91dff320
root <nathan.pankowsky@gmail.com>
2017-01-12 22:16:52 +0200

    updated process_input to store data in cache correctly

--- website/process_input.py
+++ website/process_input.py
@@ -1,9 +1,15 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 
 #process_input.py
 
 import sys
+import datetime
+import json
 
 if __name__ == "__main__":
-    print(sys.argv)
-    
+    dIn = {}
+    for arg in sys.argv[1:]:
+        key, value = arg.split(':')
+        dIn[key] = value
+    dIn['datetime'] = str(datetime.datetime.now())
+    print(json.dumps(dIn))
