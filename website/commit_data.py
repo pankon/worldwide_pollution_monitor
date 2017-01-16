27e7de0404011b8347bd2d963516c4c41b61eadf
root <nathan.pankowsky@gmail.com>
2017-01-16 22:19:06 +0200

    added data pass to commit_data.py

--- website/commit_data.py
+++ website/commit_data.py
@@ -4,11 +4,34 @@
 
 import os
 import sys
+import json
+import urllib3
+from static_config import environment_variables, write_vars
+dynamic_config = json.load(open('dynamic_config.json','r'))
 
 def commit_data():
-    for file in os.popen('ls | grep cache-*'):
-        print(file)
-    print(os.popen('ls | grep cache-*'))
+    loaded = []
+    for _file in os.popen('ls cache*').read().split('\n'):
+        if os.path.isfile(_file):
+            with open(_file) as f:
+                for line in f.readlines():
+                    loaded.append(json.loads(line))
+                only_vals_string = _file.replace('cache-', '')
+                implicit_values_dict = dict(zip(
+                    environment_variables, only_vals_string.split('_-_')
+                ))
+                loaded[-1].update(implicit_values_dict)
+    print(loaded)
+    http = urllib3.PoolManager(maxsize=len(loaded))
+    for loaded_item in loaded:
+        r = http.request(
+            'GET',
+            dynamic_config['next_url'],
+            fields=loaded_item
+        )
+        print(r)
+
+
 
 if __name__ == "__main__":
     commit_data()
